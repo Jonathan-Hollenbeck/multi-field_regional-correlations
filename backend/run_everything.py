b2019f7d2e1e2ce5abb3de4db5f312a4819eeb53
Jonathan Hollenbeck <j_holl14@wwu.de>
2023-09-09 13:41:24 +0200

    deleted uneccessary python files

--- backend/run_everything.py
+++
@@ -1,9 +0,0 @@
-import multifield_correlate_multiple_fields
-print("\n")
-import preprocessing_segmentation
-print("\n")
-import add_hulls
-print("\n")
-import add_minmax
-print("\n")
-import add_color
